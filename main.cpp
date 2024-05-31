d2ff3384dcf86a29dffce1176856270ba6a3a558
martynasmat <martynasmat03@gmail.com>
2024-05-31 16:10:14 +0300

    cross-reference lentele

--- main.cpp
+++ main.cpp
@@ -4,15 +4,15 @@
 #include <fstream>
 #include <algorithm>
 #include <sstream>
-#include <vector>
 #include <iomanip>
+#include <set>
 
 using namespace std;
 
 int main() {
     map<string, int> word_map;
     multimap<string, int> line_map;
-    vector <string> urls;
+    set<string> word_set;
 
     ifstream file("../text.txt");
     if(!file) {
@@ -20,7 +20,7 @@ int main() {
         return -1;
     }
 
-    ofstream output_count("word_count.txt");
+    ofstream output_count("../word_count.txt");
 
     string line;
     int line_number = 0;
@@ -42,6 +42,7 @@ int main() {
             word_map[word]++;
             output_count << word << " " << word_map[word] << endl;
 
+            word_set.insert(word);
             line_map.insert(pair<string, int> (word, line_number));
         }
     }
@@ -53,7 +54,28 @@ int main() {
     for(int i = 1; i <= line_number; i++) {
         output_table << left << setw(to_string(line_number).length() + 2) << i;
     }
-    output_table.close();
+    output_table << endl;
+    for (const auto& word : word_set) {
+        if (word_map[word] > 1) {
+            output_table << setw(25) << left << word;
+            for (int i = 1; i <= line_number; i++) {
+                auto range = line_map.equal_range(word);
+                bool found = false;
+                for (auto it = range.first; it != range.second; ++it) {
+                    if (it->second == i) {
+                        output_table << setw(4) << "X";
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (!found) {
+                    output_table << setw(4) << " ";
+                }
+            }
+            output_table << endl;
+        }
+    }
 
     return 0;
 }
